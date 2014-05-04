a09a257835b6558cb8c8761a2999e48a86576034
Karol <karol.s.baginski@gmail.com>
2014-05-05 00:59:43 +0100

    Finish sign in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -18,7 +18,7 @@ describe "Authentication" do
 			before {click_button "Sign in"}
 
 			it {should have_title("Sign in")}
-			it {should should have_error_message('Invalid')}
+			it {should have_error_message('Invalid')}
 
 			describe "after visiting another page" do
 				before {click_link "Home"}
